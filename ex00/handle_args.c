a64cafbf161969686c1d65e0cd3a8254fa0375a6
aLeuleu <aurelien.levra@gmail.com>
2022-07-17 17:46:49 +0200

    modify handle_args.c

--- ex00/handle_args.c
+++ ex00/handle_args.c
@@ -3,122 +3,90 @@
 /*                                                        :::      ::::::::   */
 /*   handle_args.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ldurieux <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: alevra <alevra@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/07/16 05:29:40 by ldurieux          #+#    #+#             */
-/*   Updated: 2022/07/16 07:49:46 by ldurieux         ###   ########lyon.fr   */
+/*   Created: 2022/07/17 10:21:51 by alevra            #+#    #+#             */
+/*   Updated: 2022/07/17 17:33:02 by alevra           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include<stdlib.h>
 
-int		ft_atoi(char **str);
-int		contains_other_than(char *str, char *search);
-void	free_array(int **array, int size);
-int		ft_is_white_space(char c);
+int	is_even(int i);
+int	char_is_valid_number(char c);
+int	char_is_space(char c);
+int	ft_strlen(char *str);
 
-int	get_size(char *arg, int *width, int *height)
+int	**handle_args(int argc, char **argv)
 {
-	if (!arg)
-	{
-		*width = 4;
-		*height = 4;
-		return (1);
-	}
-	*width = ft_atoi(&arg);
-	*height = ft_atoi(&arg);
-	if (*width <= 0 || *height <= 0)
-		return (0);
-	return (1);
-}
-
-int	get_board(int **board, char *str)
-{
-	int	i;
-	int	j;
+	unsigned int	i;
+	unsigned int	j;
+	int				**params;
+	void			*error;
 
 	i = 0;
+	j = 0;
+	if (argc != 2)
+		return (error);
+	params = malloc(sizeof(int *) * 4);
 	while (i < 4)
+		params[i++] = malloc(sizeof(int) * 4);
+	i = 0;
+	if (ft_strlen(argv[0]) != 31)
+		return (error);
+	if (!check_and_copy(argv[0], params))
+		return (error);
+	return (params);
+}
+
+int	check_and_copy(char *str, int **params)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	j = 0;
+	while (str[i])
 	{
-		j = 0;
-		while (j < board[0][i / 2])
+		if (!(i % 2))
 		{
-			board[i + 1][j] = ft_atoi(&str);
-			if (!board[i + 1][j])
+			if (char_is_valid_number(str[i]))
+				params[j / 4][j++ % 4] = str[i] - '0';
+			else
 				return (0);
-			j++;
 		}
+		else
+			if (!(char_is_space(str[i])))
+				return (0);
 		i++;
 	}
+}
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
 	while (*str)
-		if (!ft_is_white_space(*str++))
-			return (0);
-	return (1);
+	{
+		i ++;
+		str++;
+	}
+	return (i);
 }
 
-int	make_board(int **board)
+int	char_is_valid_number(char c)
 {
-	int	i;
-
-	i = 0;
-	while (i < 4)
-	{
-		board[i + 1] = malloc(sizeof(int) * board[0][i / 2]);
-		if (!board[i + 1])
-		{
-			free_array(board, 5);
-			return (0);
-		}
-		i++;
-	}
-	return (1);
-}
-
-int	**get_data(char *arg, int width, int height)
-{
-	int	**res;
-	int	i;
-
-	res = malloc(sizeof(int *) * 5);
-	if (!res)
-		return (0);
-	res[0] = malloc(sizeof(int) * 2);
-	if (!res[0])
-	{
-		free_array(res, 5);
-		return (0);
-	}
-	res[0][0] = width;
-	res[0][1] = height;
-	i = 0;
-	if (!make_board(res))
-		return (0);
-	if (!get_board(res, arg))
-		return (0);
-	return (res);
-}
-
-int	**handle_args(int argc, char **argv, int accept_size)
-{
-	int	width;
-	int	height;
-	int	i;
-
-	i = 1;
-	while (i < argc)
-		if (contains_other_than(argv[i++], "0123456789 "))
-			return (0);
-	if (accept_size && argc == 3)
-	{
-		if (!get_size(argv[2], &width, &height))
-			return (0);
-	}
-	else if (argc == 2)
-	{
-		if (!get_size(0, &width, &height))
-			return (0);
-	}
+	if (c >= '1' && c <= '4' )
+		return (1);
 	else
 		return (0);
-	return (get_data(argv[1], width, height));
+}
+
+int	char_is_space(char c)
+{
+	if (c == ' ')
+		return (1);
+	else
+		return (0);
 }
